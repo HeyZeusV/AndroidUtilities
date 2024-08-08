9140a0017ef8f166068a77717ad54b21fca62ede
HeyZeusV <valdesj419@gmail.com>
2024-08-08 01:16:46 -0700

    Added Compose BOM, graphics, and ui dependencies. Added ComposeResources.kt which contains shortened function names of all possible Composable *Resource functions. They don't do anything extra, just have shorter names. Will be including xml containing Live Templates that can be imported into Android Studio.

--- androidutilities/build.gradle.kts
+++ androidutilities/build.gradle.kts
@@ -33,10 +33,12 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.animation.graphics)
+    implementation(libs.androidx.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ androidutilities/src/main/java/com/heyzeusv/androidutilities/compose/util/ComposeResources.kt
@@ -0,0 +1,137 @@
+package com.heyzeusv.androidutilities.compose.util
+
+import androidx.annotation.ArrayRes
+import androidx.annotation.BoolRes
+import androidx.annotation.ColorRes
+import androidx.annotation.DimenRes
+import androidx.annotation.DrawableRes
+import androidx.annotation.IntegerRes
+import androidx.annotation.PluralsRes
+import androidx.annotation.StringRes
+import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
+import androidx.compose.animation.graphics.res.animatedVectorResource
+import androidx.compose.animation.graphics.vector.AnimatedImageVector
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.booleanResource
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.integerArrayResource
+import androidx.compose.ui.res.integerResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.pluralStringResource
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
+
+/**
+ *  Composable resource functions with shorten names.
+ *  I highly recommend pairing these with Live Templates and having "Add unambiguous imports on the
+ *  fly" turned on. Setting is found File -> Settings -> Editor -> General -> Auto Import.
+ *
+ *  TODO: Add link to xml containing LiveTemplates
+ */
+/**
+ *  Load a string resource with formatting.
+ *
+ *  @param id The resource identifier.
+ *  @param args The format arguments.
+ *  @return The string data associated with the resource.
+ */
+@Composable
+@ReadOnlyComposable
+fun sRes(@StringRes id: Int, vararg args: Any): String = stringResource(id, *args)
+
+/**
+ *  Load a string array resource.
+ *
+ *  @param id The resource identifier.
+ *  @return The string array data associated with the resource.
+ */
+@Composable
+@ReadOnlyComposable
+fun saRes(@ArrayRes id: Int): Array<String> = stringArrayResource(id)
+
+/**
+ * Load a plurals resource with provided format arguments.
+ *
+ *  @param id the resource identifier
+ *  @param count the count
+ *  @param args arguments used in the format string
+ *  @return the pluralized string data associated with the resource
+ */
+@Composable
+@ReadOnlyComposable
+fun psRes(@PluralsRes id: Int, count: Int, vararg args: Any): String =
+    pluralStringResource(id, count, *args)
+
+/**
+ *  Create a [Painter] from an Android resource id.
+ *
+ *  @param id Resources object to query the image file from.
+ *  @return [Painter] used for drawing the loaded resource.
+ */
+@Composable
+fun pRes(@DrawableRes id: Int): Painter = painterResource(id)
+
+/**
+ *  Load a dimension resource.
+ *
+ *  @param id The resource identifier.
+ *  @return The dimension value associated with the resource.
+ */
+@Composable
+@ReadOnlyComposable
+fun dRes(@DimenRes id: Int): Dp = dimensionResource(id)
+
+/**
+ *  Load a boolean resource.
+ *
+ *  @param id the resource identifier
+ *  @return the boolean associated with the resource
+ */
+@Composable
+@ReadOnlyComposable
+fun bRes(@BoolRes id: Int): Boolean = booleanResource(id)
+
+/**
+ *  Load a integer resource.
+ *
+ *  @param id The resource identifier.
+ *  @return The integer value associated with the resource.
+ */
+@Composable
+@ReadOnlyComposable
+fun iRes(@IntegerRes id: Int): Int = integerResource(id)
+
+/**
+ *  Load an array of integer resource.
+ *
+ *  @param id the resource identifier
+ *  @return the integer array associated with the resource
+ */
+@Composable
+@ReadOnlyComposable
+fun iaRes(@ArrayRes id: Int): IntArray = integerArrayResource(id)
+
+/**
+ *  Load a color resource.
+ *
+ *  @param id the resource identifier
+ *  @return the color associated with the resource
+ */
+@Composable
+@ReadOnlyComposable
+fun cRes(@ColorRes id: Int): Color = colorResource(id)
+
+/**
+ *  Load an [AnimatedImageVector] from an Android resource id.
+ *
+ *  @param id the resource identifier
+ *  @return an animated vector drawable resource.
+ */
+@OptIn(ExperimentalAnimationGraphicsApi::class)
+@Composable
+fun avRes(@DrawableRes id: Int): AnimatedImageVector = AnimatedImageVector.animatedVectorResource(id)
