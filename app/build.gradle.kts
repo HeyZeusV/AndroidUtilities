5a2f9d7e71645798b00d07e74b364a702afafa6f
HeyZeusV <valdesj419@gmail.com>
2024-08-08 05:09:30 -0700

    Added Compose animation dependency to sample app in order to display AnimatedImageVector. Added Compose gradle options in library build.gradle. Created very simple screen to display all ComposeResources functions. Small fixes to resources used.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
+    implementation(libs.androidx.animation.graphics)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
