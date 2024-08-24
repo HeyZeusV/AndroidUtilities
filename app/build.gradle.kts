e68eec163837effeea47f7c3581209c5a6cb49f9
HeyZeusV <valdesj419@gmail.com>
2024-08-24 01:19:37 -0700

    Current build was failing on JitPack due to JitPack trying to build release version of sample app and failing due to not having access to signing config. So removed signing config.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,4 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import java.io.FileInputStream
-import java.util.Properties
 
 plugins {
     alias(libs.plugins.android.application)
@@ -11,19 +9,6 @@ plugins {
 }
 
 android {
-    signingConfigs {
-        create("release") {
-            val props = Properties()
-            val fileInputStream = FileInputStream(file("../private/signing.properties"))
-            props.load(fileInputStream)
-            fileInputStream.close()
-
-            storeFile = file(props["storeFilePath"] as String)
-            storePassword = props["storePassword"] as String
-            keyPassword = props["keyPassword"] as String
-            keyAlias = props["keyAlias"] as String
-        }
-    }
     namespace = "com.heyzeusv.androidutilitieslibrary"
     compileSdk = 34
 
@@ -47,7 +32,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("release")
+            signingConfig = signingConfigs.getByName("debug")
         }
         debug {
             applicationIdSuffix = ".dev"
