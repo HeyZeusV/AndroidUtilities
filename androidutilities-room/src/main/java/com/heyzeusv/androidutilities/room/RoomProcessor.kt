c08d7184e09c7acb5a6892845ad83c6ecf34606f
HeyZeusV <valdesj419@gmail.com>
2024-09-04 18:00:37 -0700

    Classes are now being created correctly without any annotations or extras. Create constructor and properties using TypeSpec.Builder extension functions. Skip any parameter and property with @Ignore annotation.

--- androidutilities-room/src/main/java/com/heyzeusv/androidutilities/room/RoomProcessor.kt
+++ androidutilities-room/src/main/java/com/heyzeusv/androidutilities/room/RoomProcessor.kt
@@ -1,6 +1,7 @@
 package com.heyzeusv.androidutilities.room
 
 import com.google.devtools.ksp.processing.Dependencies
+import com.google.devtools.ksp.processing.KSPLogger
 import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
@@ -8,6 +9,11 @@ import com.google.devtools.ksp.symbol.KSAnnotated
 import com.google.devtools.ksp.symbol.KSClassDeclaration
 import com.google.devtools.ksp.validate
 import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.PropertySpec
+import com.squareup.kotlinpoet.TypeSpec
+import com.squareup.kotlinpoet.ksp.toTypeName
 
 class RoomProcessor(private val environment: SymbolProcessorEnvironment) : SymbolProcessor {
     private val logger = environment.logger
@@ -16,42 +22,33 @@ class RoomProcessor(private val environment: SymbolProcessorEnvironment) : Symbo
         // get all symbols with Entity
         val symbols = resolver.getSymbolsWithAnnotation("androidx.room.Entity")
             .plus(resolver.getSymbolsWithAnnotation("androidx.room.ColumnInfo"))
+            .plus(resolver.getSymbolsWithAnnotation("androidx.room.Ignore"))
             .toSet()
 
         // filter out symbols that are not classes
         symbols.filterIsInstance<KSClassDeclaration>().forEach { symbol ->
             (symbol as? KSClassDeclaration)?.let { classDeclaration ->
                 val packageName = classDeclaration.containingFile?.packageName?.asString().orEmpty()
-                val className = classDeclaration.simpleName.getShortName()
-                val fileName = "${className}Impl"
+                val className = "${classDeclaration.simpleName.getShortName()}RoomUtil"
 
                 logger.info("class name: $className")
-                val declarations = classDeclaration.containingFile?.declarations
-                declarations?.forEach { logger.info("declarations $it") }
-                symbol.primaryConstructor?.parameters?.forEach { parameter ->
-                    val name = parameter.name?.getShortName() ?: "empty name"
-                    logger.info("parameter name: ${name}, type: ${parameter.type}")
-                }
-                symbol.getAllProperties().forEach { prop ->
-                    val name = prop.qualifiedName?.getShortName()
-                    logger.info("property name: ${name}, type: ${prop.type}")
-                }
 
                 // use KotlinPoet for code generation
-                val fileSpecBuilder = FileSpec.builder(packageName, fileName)
+                val fileSpecBuilder = FileSpec.builder(packageName, className)
 
-//                // create copy of class
-//                val classBuilder = TypeSpec.classBuilder(className)
-//                    .addModifiers(KModifier.DATA)
-//                    .primaryConstructor(classDeclaration)
-//
-//                fileSpecBuilder.addType(classBuilder.build())
+                // create copy of class
+                val classBuilder = TypeSpec.classBuilder(className)
+                    .addModifiers(KModifier.DATA)
+                    .primaryConstructor(classDeclaration, logger)
+                    .properties(classDeclaration, logger)
+
+                fileSpecBuilder.addType(classBuilder.build())
 
                 // writing the file
                 environment.codeGenerator.createNewFile(
                     dependencies = Dependencies(false, symbol.containingFile!!),
                     packageName = packageName,
-                    fileName = fileName,
+                    fileName = className,
                     extensionName = "kt",
                 ).bufferedWriter().use {
                     fileSpecBuilder.build().writeTo(it)
@@ -65,25 +62,40 @@ class RoomProcessor(private val environment: SymbolProcessorEnvironment) : Symbo
     }
 }
 
-//private fun TypeSpec.Builder.primaryConstructor(
-//    classDeclaration: KSClassDeclaration
-//): TypeSpec.Builder {
-//    val cBuilder = FunSpec.constructorBuilder()
-//    classDeclaration.primaryConstructor?.parameters?.forEach { parameter ->
-//
-//        ParameterSpec.builder("ajd", String::class).build()
-//        cBuilder.addParameter(parameter.name.toString())
-//    }
-//    val t = classDeclaration.primaryConstructor?.parameters?.first()
-//    t?.
-//    classDeclaration.getAllProperties().forEach { prop ->
-//        cBuilder.addParameter(prop.simpleName.toString(),)
-//    }
-//    this.primaryConstructor(
-//        FunSpec.constructorBuilder()
-//
-//            .build()
-//    )
-//
-//    return this
-//}
+private fun TypeSpec.Builder.primaryConstructor(
+    classDeclaration: KSClassDeclaration,
+    logger: KSPLogger,
+): TypeSpec.Builder {
+    val cBuilder = FunSpec.constructorBuilder()
+    classDeclaration.primaryConstructor?.parameters?.forEach { parameter ->
+        parameter.name?.getShortName()?.let { name ->
+            if (!parameter.annotations.any { it.shortName.getShortName() == "Ignore" }) {
+                logger.info("parameter name: ${name}, type: ${parameter.type}")
+
+                cBuilder.addParameter(name, parameter.type.toTypeName())
+            } else {
+                logger.info("ignored! name: $name}")
+            }
+        }
+    }
+    this.primaryConstructor(cBuilder.build())
+
+    return this
+}
+
+private fun TypeSpec.Builder.properties(
+    classDeclaration: KSClassDeclaration,
+    logger: KSPLogger,
+): TypeSpec.Builder {
+    classDeclaration.getAllProperties().forEach { prop ->
+        prop.qualifiedName?.getShortName()?.let { name ->
+            if (!prop.annotations.any { it.shortName.getShortName() == "Ignore" }) {
+                logger.info("property name: ${name}, type: ${prop.type}")
+                this.addProperty(PropertySpec.builder(name, prop.type.toTypeName()).initializer(name).build())
+            } else {
+                logger.info("ignored! name: $name")
+            }
+        }
+    }
+    return this
+}
