20e2647f27748de289403bbbd85f98c125c4ddd4
HeyZeusV <valdesj419@gmail.com>
2024-09-26 05:32:45 -0700

    Add flows for Categories and DefaultItems in Database. Add functions to add 10 Categories/DefaultItems with random values.

--- app/src/main/java/com/heyzeusv/androidutilitieslibrary/feature/roomutil/RoomUtilViewModel.kt
+++ app/src/main/java/com/heyzeusv/androidutilitieslibrary/feature/roomutil/RoomUtilViewModel.kt
@@ -7,10 +7,17 @@ import com.heyzeusv.androidutilitieslibrary.database.CsvConverter
 import com.heyzeusv.androidutilitieslibrary.database.Repository
 import com.heyzeusv.androidutilitieslibrary.database.RoomBackupRestore
 import com.heyzeusv.androidutilitieslibrary.database.RoomData
+import com.heyzeusv.androidutilitieslibrary.database.models.Category
+import com.heyzeusv.androidutilitieslibrary.database.models.DefaultItem
+import com.heyzeusv.androidutilitieslibrary.database.models.SampleInnerEmbed
+import com.heyzeusv.androidutilitieslibrary.database.models.SampleOuterEmbed
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.random.Random
 
 @HiltViewModel
 class RoomUtilViewModel @Inject constructor(
@@ -18,6 +25,22 @@ class RoomUtilViewModel @Inject constructor(
     private val roomBackupRestore: RoomBackupRestore,
     private val csvConverter: CsvConverter,
 ) : ViewModel() {
+
+    private val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+
+    private val categories = repository.getAllCategoriesFlow()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(),
+            initialValue = emptyList(),
+        )
+
+    private val items = repository.getAllDefaultItems()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(),
+            initialValue = emptyList(),
+        )
 
     private var _appDbDirectoryUri: Uri? = null
     val appDbDirectoryUri: Uri? get() = _appDbDirectoryUri
@@ -29,6 +52,63 @@ class RoomUtilViewModel @Inject constructor(
         _appDbDirectoryUri = null
         _appCsvDirectoryUri = null
 
+    }
+
+    /**
+     *  Create string of random characters with given [size].
+     */
+    private fun randomString(size: Int = 8) = List(size) { charPool.random() }.joinToString("")
+
+    /**
+     *  Add 10 Categories to database with random name.
+     */
+    fun add10RandomCategories() {
+        viewModelScope.launch {
+            val categoryList = mutableListOf<Category>()
+            repeat(10) {
+                val category = Category(name = randomString())
+                categoryList.add(category)
+            }
+            repository.insertCategories(*categoryList.toTypedArray())
+        }
+    }
+
+    /**
+     *  Add 10 Items to database with mostly random values.
+     */
+    fun add10RandomDefaultItems() {
+        viewModelScope.launch {
+            val itemList = mutableListOf<DefaultItem>()
+            repeat(10) {
+                val item = DefaultItem(
+                    name = randomString(),
+                    category = categories.value.random().name,
+                    quantity = Random.nextDouble(),
+                    unit = randomString(4),
+                    memo = randomString(32),
+                    outerEmbed = SampleOuterEmbed(
+                        sameName = randomString(),
+                        someField = Random.nextInt(),
+                        uselessField = Random.nextLong(),
+                        embed = SampleInnerEmbed(
+                            sameName = randomString(),
+                            nullableBoolean = Random.nextBoolean(),
+                            nullableShort = null,
+                            nullableInt = Random.nextInt(),
+                            nullableLong = Random.nextLong(),
+                            nullableByte = null,
+                            nullableString = randomString(),
+                            nullableChar = null,
+                            nullableDouble = Random.nextDouble(),
+                            nullableFloat = Random.nextFloat(),
+                            nullableByteArray = Random.nextBytes(4)
+                        )
+                    )
+                )
+                itemList.add(item)
+            }
+            repository.upsertDefaultItems(*itemList.toTypedArray())
+        }
     }
 
     fun restoreDatabase(selectedDirectoryUri: Uri) {
