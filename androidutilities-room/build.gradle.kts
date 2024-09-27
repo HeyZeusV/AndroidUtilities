7b46e84ac6a309b943e22e55cf31303b9319cd35
HeyZeusV <valdesj419@gmail.com>
2024-09-27 07:34:42 -0700

    Add test dependencies for room-util module

--- androidutilities-room/build.gradle.kts
+++ androidutilities-room/build.gradle.kts
@@ -24,4 +24,9 @@ dependencies {
 
     // KSP
     implementation("com.google.devtools.ksp:symbol-processing-api:2.0.0-1.0.21")
+
+    testImplementation(kotlin("test"))
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("com.github.tschuchortdev:kotlin-compile-testing:1.6.0")
+    testImplementation("com.github.tschuchortdev:kotlin-compile-testing-ksp:1.6.0")
 }
