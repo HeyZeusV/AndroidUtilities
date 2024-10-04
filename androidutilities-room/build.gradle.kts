51ff2ced84bd47e58d239269af53320397248dc7
HeyZeusV <valdesj419@gmail.com>
2024-10-04 08:48:19 -0700

    Update to use version catalog.

--- androidutilities-room/build.gradle.kts
+++ androidutilities-room/build.gradle.kts
@@ -18,14 +18,14 @@ kotlin {
 }
 dependencies {
     // KotlinPoet
-    implementation("com.squareup:kotlinpoet:1.15.0")
-    implementation("com.squareup:kotlinpoet-ksp:1.15.0")
+    implementation(libs.kotlinpoet)
+    implementation(libs.kotlinpoet.ksp)
 
     // KSP
-    implementation("com.google.devtools.ksp:symbol-processing-api:2.0.0-1.0.21")
+    implementation(libs.symbol.processing.api)
 
     testImplementation(kotlin("test"))
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("com.github.tschuchortdev:kotlin-compile-testing:1.6.0")
-    testImplementation("com.github.tschuchortdev:kotlin-compile-testing-ksp:1.6.0")
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlin.compile.testing)
+    testImplementation(libs.kotlin.compile.testing.ksp)
 }
